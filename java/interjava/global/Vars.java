e31022669a918914fcb65d4d28efbf01b0781468
Andrey Slobodchikov <andslob@inbox.ru>
2023-06-25 21:09:07 +0500

    done

---
+++ java/interjava/global/Vars.java
@@ -0,0 +1,12 @@
+package intermediate_control_work.java.interjava.global;
+
+import intermediate_control_work.java.interjava.model.Toy;
+
+import java.util.ArrayList;
+
+public class Vars {
+    public static ArrayList<Toy> allToys = new ArrayList<Toy>();
+    public static ArrayList<Toy> winsToys = new ArrayList<Toy>();
+
+    public static String myFileName = "winnerTxt.txt";
+}
